d704209636736a57748f42f3c6e9cffaec4c60c6
PhPeKe <p.kerstenlive.de>
2018-02-09 23:36:03 +0100

    Final version, i tried to do the Memory managment part but it crashed all the time with the things i tried, so i commented my attempts.

--- app/src/main/java/com/example/phillip/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/phillip/mrpotatohead/MainActivity.java
@@ -9,21 +9,45 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ImageView image = findViewById(R.id.arms);
+        ImageView image2 = findViewById(R.id.hat);
+        ImageView image3 = findViewById(R.id.glasses);
+        ImageView image4 = findViewById(R.id.mustache);
+        ImageView image5 = findViewById(R.id.shoes);
+        ImageView image10 = findViewById(R.id.ears);
+        ImageView image6 = findViewById(R.id.nose);
+        ImageView image7 = findViewById(R.id.mouth);
+        ImageView image8 = findViewById(R.id.eyebrows);
+        ImageView image9 = findViewById(R.id.eyes);
+
         setContentView(R.layout.activity_main);
+        //image.setVisibility(savedInstanceState.getInt("image"));
+        //image2.setVisibility(savedInstanceState.getInt("image2"));
+        //image2.setVisibility(savedInstanceState.getInt("image3"));
+        //image2.setVisibility(savedInstanceState.getInt("image4"));
+        //image2.setVisibility(savedInstanceState.getInt("image5"));
+        //image2.setVisibility(savedInstanceState.getInt("image6"));
+        //image2.setVisibility(savedInstanceState.getInt("image7"));
+        //image2.setVisibility(savedInstanceState.getInt("image8"));
+        //image2.setVisibility(savedInstanceState.getInt("image9"));
+        //image2.setVisibility(savedInstanceState.getInt("image10"));
+
     }
 
     public void checkClicked(View v) {
         Log.d(  "potato",  "checkClicked");
         CheckBox checkbox = (CheckBox) v;
         String boxid = checkbox.getText().toString();
-        Log.d("potato", boxid);
-        //ImageView image = findViewById(R.id.arms);
+
         switch(boxid) {
             case "arms":
                 ImageView image = findViewById(R.id.arms);
+
                 if (checkbox.isChecked()) {
                     image.setVisibility(View.VISIBLE);
                 }
@@ -34,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "hat":
                 ImageView image2 = findViewById(R.id.hat);
+
                 if (checkbox.isChecked()) {
                     image2.setVisibility(View.VISIBLE);
                 }
@@ -44,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "glasses":
                 ImageView image3 = findViewById(R.id.glasses);
+
                 if (checkbox.isChecked()) {
                     image3.setVisibility(View.VISIBLE);
                 }
@@ -54,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "mustache":
                 ImageView image4 = findViewById(R.id.mustache);
+
                 if (checkbox.isChecked()) {
                     image4.setVisibility(View.VISIBLE);
                 }
@@ -64,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "shoes":
                 ImageView image5 = findViewById(R.id.shoes);
+
                 if (checkbox.isChecked()) {
                     image5.setVisibility(View.VISIBLE);
                 }
@@ -74,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "ears":
                 ImageView image10 = findViewById(R.id.ears);
+
                 if (checkbox.isChecked()) {
                     image10.setVisibility(View.VISIBLE);
                 }
@@ -84,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "nose":
                 ImageView image6 = findViewById(R.id.nose);
+
                 if (checkbox.isChecked()) {
                     image6.setVisibility(View.VISIBLE);
                 }
@@ -94,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "mouth":
                 ImageView image7 = findViewById(R.id.mouth);
+
                 if (checkbox.isChecked()) {
                     image7.setVisibility(View.VISIBLE);
                 }
@@ -104,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "eyebrows":
                 ImageView image8 = findViewById(R.id.eyebrows);
+
                 if (checkbox.isChecked()) {
                     image8.setVisibility(View.VISIBLE);
                 }
@@ -114,6 +146,7 @@ public class MainActivity extends AppCompatActivity {
 
             case "eyes":
                 ImageView image9 = findViewById(R.id.eyes);
+
                 if (checkbox.isChecked()) {
                     image9.setVisibility(View.VISIBLE);
                 }
@@ -121,8 +154,34 @@ public class MainActivity extends AppCompatActivity {
                     image9.setVisibility(View.INVISIBLE);
                 }
                 break;
-
         }
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        ImageView image = findViewById(R.id.arms);
+        ImageView image2 = findViewById(R.id.hat);
+        ImageView image3 = findViewById(R.id.glasses);
+        ImageView image4 = findViewById(R.id.mustache);
+        ImageView image5 = findViewById(R.id.shoes);
+        ImageView image10 = findViewById(R.id.ears);
+        ImageView image6 = findViewById(R.id.nose);
+        ImageView image7 = findViewById(R.id.mouth);
+        ImageView image8 = findViewById(R.id.eyebrows);
+        ImageView image9 = findViewById(R.id.eyes);
+
+
+        outState.putInt("image", image.getVisibility());
+        outState.putInt("image2", image2.getVisibility());
+        outState.putInt("image3", image3.getVisibility());
+        outState.putInt("image4", image4.getVisibility());
+        outState.putInt("image5", image5.getVisibility());
+        outState.putInt("image6", image6.getVisibility());
+        outState.putInt("image7", image7.getVisibility());
+        outState.putInt("image8", image8.getVisibility());
+        outState.putInt("image9", image9.getVisibility());
+        outState.putInt("image10", image10.getVisibility());
 
     }
 }
